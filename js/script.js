0c3f122fb40115eb65974560ceec5cac0e203947
Ianadika44 <adika19ian@gmail.com>
2020-02-24 11:33:53 +0300

    togled the classes design,devpt and product

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
+$(document).ready(function() {
+    $(".design").click(function() {
+        $(".p1").toggle();
+        $(".image1").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".devpt").click(function(){
+        $(".p2").toggle();
+        $(".image2").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".product").click(function(){
+        $(".p3").toggle();
+        $(".image3").toggle
+    });
+});
 
-// function myFunction (){
-//     var name = document.getElementById("name").value;
-//     if (name !== ""){
-//         alert(name + " we have received your email");
-//         } else if (name == "") {
-//         alert(name + " enter valid address");
-//         } else {
-//             alert("Fill your form correctly!");
-//         };
-// }
+function myFunction (){
+    var name = document.getElementById("name").value;
+    if (name !== ""){
+        alert(name + " we have received your email");
+        } else if (name == "") {
+        alert(name + " enter valid address");
+        } else {
+            alert("Fill your form correctly!");
+        };
+}
