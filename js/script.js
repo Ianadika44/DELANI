bfc61868c619075ee80e69719c88db8fa3d4a173
Ianadika44 <adika19ian@gmail.com>
2020-02-24 10:26:09 +0300

    made changes in the div class social..removed the table to use div for the layout

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-function myFunction(name){
-   var name=document.getElementById("name").value;
-   if (name!==null){
-       alert(name + " we have received your mail")
-   }
-   return name;
-}
+
+// function myFunction (){
+//     var name = document.getElementById("name").value;
+//     if (name !== ""){
+//         alert(name + " we have received your email");
+//         } else if (name == "") {
+//         alert(name + " enter valid address");
+//         } else {
+//             alert("Fill your form correctly!");
+//         };
+// }
