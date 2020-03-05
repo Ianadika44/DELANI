fb08bdbaf84727fe9b56bbec743d0bc2fb3b0571
Ianadika44 <adika19ian@gmail.com>
2020-03-05 12:15:02 +0300

    added responsiveness in css to fit in any device and hovering effect in js

--- js/script.js
+++ js/script.js
@@ -16,11 +16,6 @@ $(document).ready(function(){
         $(".p3").toggle();
     });
 });
-// $("document").hover(function(){
-//     $(tc).css("background-color", "white");
-//     }, function(){
-//     $(tc).css("background-color", "white");
-//   });
 
 function myFunction (){
     var name = document.getElementById("name").value;
@@ -32,3 +27,78 @@ function myFunction (){
             alert("Fill your form correctly!");
         };
 }
+$(document).ready(function() {
+    $("#image1")
+      .mouseover(function() {
+        $("#par1").show();
+      })
+      .mouseout(function() {
+        $("#par1").hide();
+      });
+  })
+  $(document).ready(function() {
+    $("#image2")
+      .mouseover(function() {
+        $("#par2").show();
+      })
+      .mouseout(function() {
+        $("#par2").hide();
+      });
+  })
+  $(document).ready(function() {
+    $("#image3")
+      .mouseover(function() {
+        $("#par3").show();
+      })
+      .mouseout(function() {
+        $("#par3").hide();
+      });
+  })
+  $(document).ready(function() {
+    $("#image4")
+      .mouseover(function() {
+        $("#par4").show();
+      })
+      .mouseout(function() {
+        $("#par4").hide();
+      });
+  })
+  $(document).ready(function() {
+    $("#image5")
+      .mouseover(function() {
+        $("#par5").show();
+      })
+      .mouseout(function() {
+        $("#par5").hide();
+      });
+  })
+  $(document).ready(function() {
+    $("#image6")
+      .mouseover(function() {
+        $("#par6").show();
+      })
+      .mouseout(function() {
+        $("#par6").hide();
+      });
+  })
+  $(document).ready(function() {
+    $("#image7")
+      .mouseover(function() {
+        $("#par7").show();
+      })
+      .mouseout(function() {
+        $("#par7").hide();
+      });
+  })
+  
+  $(document).ready(function() {
+    $("#image8")
+      .mouseover(function() {
+        $("#par8").show();
+      })
+      .mouseout(function() {
+        $("#par8").hide();
+      });
+  })
+  
+
